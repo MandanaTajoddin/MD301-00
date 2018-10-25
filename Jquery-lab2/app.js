1b18a190028177a2d2b08806f37ab93feeb558ee
MandanaTajoddin <40220065+MandanaTajoddin@users.noreply.github.com>
2018-10-25 18:09:57 -0400

    Jquery

--- Jquery-lab2/app.js
+++ Jquery-lab2/app.js
@@ -1,6 +1,6 @@
 const $main = $('main')
 
-const $selector = $('image-filter')
+const $selector = $('#image-filter')
 
 const apiURL =
 'https://raw.githubusercontent.com/CodePartnersMD/MD301-01/master/02-jquery-selectors-events/lab/page-1.json'
@@ -8,20 +8,25 @@ const apiURL =
 const Unicorn = function (imageObj) {
    this.name = imageObj.name;
    this.image_url = imageObj.image_url;
-   this.hobbies = imageObj.hobbies; 
+   this.hobbies = imageObj.hobbies;
+   this.title = imageObj.title; 
 };
 
-let allUnicorn = [];
+let allUnicorns = [];
 
 Unicorn.prototype.renderUnicorn = imageObj => {
     
     let $unicornClone = $('#image-template').clone()
     $main.append($unicornClone)
+    $unicornClone.attr('id',imageObj.title)
     $unicornClone.find('img').attr('src',imageObj.image_url)
+    $unicornClone.find('p').text(imageObj.title)
 
 }
 
 $($selector).on('change', () =>{
+  $('section').hide()
+  $(`section[id=${event.target.value}]`).show()
 
 })
 
@@ -29,9 +34,9 @@ $($selector).on('change', () =>{
 $.getJSON(apiURL,response => {
     response.forEach((val) => {
         let newUnicorn = new Unicorn(val)
-        allUnicorn.push(newUnicorn)
+        allUnicorns.push(newUnicorn)
         newUnicorn.renderUnicorn(val)
-        $selector.append('<option value = ${newUnicorn.name}>${newUnicorn.neme}></option>')
+        $selector.append(`<option value=${newUnicorn.title}>${newUnicorn.title}</option>`)
     })
     
 })
